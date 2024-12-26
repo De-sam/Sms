b02cc609579bbe22d815aa9180be4cffd7cedbbe
De-sam <samsoncoded@gmail.com>
2024-12-26 02:39:00 +0100

    Added functionality for forgotten passwords

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -246,4 +246,4 @@ AXES_COOLOFF_TIME = 0.5 # Lockout duration in hours
 AXES_HANDLER = 'axes.handlers.database.AxesDatabaseHandler'
 AXES_RESET_ON_SUCCESS = True  # Reset attempts after a successful login
 AXES_LOCKOUT_PARAMETERS = ['username']
-AXES_LOCKOUT_PARAMETERS = ['ip_address']
+# AXES_LOCKOUT_PARAMETERS = ['ip_address']
